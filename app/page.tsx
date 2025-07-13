2bc0abb50af784960772e811365daad314b1e3c0
Gebeya Dala <noreply@gebeya-dala.com>
2025-07-13 09:58:09 +0300

    Update files from Gebeya Dala

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import React from "react";
+
+export default function Page() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-white">
+      <header className="text-4xl font-bold text-gray-900">hello</header>
+    </main>
+  );
+}
